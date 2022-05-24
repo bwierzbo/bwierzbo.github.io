7ab450674163a3767667c4e16c7ea2b914aadad0
Benjamin Wierzbanowski <bwierzbo@gmail.com>
2022-05-24 10:27:34 -0400

    added js file and navbar

---
+++ resources/javascript/index.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.position = "fixed";
+  }
+}
